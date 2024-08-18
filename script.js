7061c2a7054f035ae40bf798aff3295ff9833a68
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-08-18 23:26:06 +0300

    Adding Country Names

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ conBtn.addEventListener("click", (el) => {
   // Using if statement
   // if (userChoice.value === "USD") {
   //   finalAmount.value = amount * 48.86;
+  // } else if (userChoice.value === "EUR") {
+  //   finalAmount.value = amount * 53.9;
   // } else if (userChoice.value === "SAR") {
   //   finalAmount.value = amount * 13.02;
   // } else if (userChoice.value === "AED") {
   //   finalAmount.value = amount * 13.3;
   // } else if (userChoice.value === "KWD") {
   //   finalAmount.value = amount * 160.5;
-  // } else if (userChoice.value === "EUR") {
-  //   finalAmount.value = amount * 53.9;
   // } else if (userChoice.value === "QAR") {
   //   finalAmount.value = amount * 13.42;
   // }
@@ -37,6 +37,9 @@ conBtn.addEventListener("click", (el) => {
     case "USD":
       finalAmount.value = amount * 48.86;
       break;
+    case "EUR":
+      finalAmount.value = amount * 53.9;
+      break;
     case "SAR":
       finalAmount.value = amount * 13.02;
       break;
@@ -45,9 +48,6 @@ conBtn.addEventListener("click", (el) => {
       break;
     case "KWD":
       finalAmount.value = amount * 160.5;
-      break;
-    case "EUR":
-      finalAmount.value = amount * 53.9;
       break;
     case "QAR":
       finalAmount.value = amount * 13.42;
